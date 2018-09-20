069bc48ccd5cdb8b0f1e048845dc906a44cc80e4
Vitor Fernandes de Carvalho <df82573es@df.trf1.gov.br>
2018-09-20 14:24:41 -0300

    criando o painel

--- resources/views/site/index.blade.php
+++ resources/views/site/index.blade.php
@@ -7,5 +7,102 @@
 @stop
 
 @section('content')
-    
+<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.2.0/css/all.css" integrity="sha384-hWVjflwFxL6sNzntih27bfxkr27PmbbK/iSvJ+a4+0owXq79v+lsFkW54bOGbiDQ" crossorigin="anonymous">
+<div>
+<div class="col-sm-15 col-md-6">
+    <div class="box box-solid">
+        <div class="box-body">
+            <br>
+            <div class="media">
+                <div class="media-left">
+                    <a href="/equipamento" class="ad-click-event">
+                        <i class="fas fa-laptop fa-6x"></i>
+                    </a>
+                </div>
+                <div class="media-right">
+                    <div class="clearfix">
+                        <h3 style=" font-size: 27px; text-align: left; padding: 7px 30px; margin-top: 0 ;">Equipamento</h3>
+
+                        <h2 style=" font-size: 14px; text-align: left; padding: 7px 30px; margin-top: 0 ;">Acessar funcionalidades de equipamento</h2>
+                        <br>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<div>
+<div class="col-sm-15 col-md-6">
+    <div class="box box-solid">
+        <div class="box-body">
+            <br>
+            <div class="media">
+                <div class="media-left">
+                    <a href="/equipamento" class="ad-click-event">
+                        <i class="fas fa-laptop fa-6x"></i>
+                    </a>
+                </div>
+                <div class="media-right">
+                    <div class="clearfix">
+                        <h3 style=" font-size: 27px; text-align: left; padding: 7px 30px; margin-top: 0 ;">Equipamento</h3>
+
+                        <h2 style=" font-size: 14px; text-align: left; padding: 7px 30px; margin-top: 0 ;">Acessar funcionalidades de equipamento</h2>
+                        <br>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<div>
+<div class="col-sm-15 col-md-6">
+    <div class="box box-solid">
+        <div class="box-body">
+            <br>
+            <div class="media">
+                <div class="media-left">
+                    <a href="/equipamento" class="ad-click-event">
+                        <i class="fas fa-laptop fa-6x"></i>
+                    </a>
+                </div>
+                <div class="media-right">
+                    <div class="clearfix">
+                        <h3 style=" font-size: 27px; text-align: left; padding: 7px 10px; margin-top: 0 ;">Equipamento</h3>
+
+                        <h2 style=" font-size: 14px; text-align: left; padding: 7px 10px; margin-top: 0 ;">Acessar funcionalidades de equipamento</h2>
+                        <br>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<div>
+<div class="col-sm-15 col-md-6">
+    <div class="box box-solid">
+        <div class="box-body">
+            <br>
+            <div class="media">
+                <div class="media-left">
+                    <a href="/equipamento" class="ad-click-event">
+                        <i class="fas fa-laptop fa-6x"></i>
+                    </a>
+                </div>
+                <div class="media-right">
+                    <div class="clearfix">
+                        <h3 style=" font-size: 27px; text-align: left; padding: 7px 10px; margin-top: 0 ;">Equipamento</h3>
+
+                        <h2 style=" font-size: 14px; text-align: left; padding: 7px 10px; margin-top: 0 ;">Acessar funcionalidades de equipamento</h2>
+                        <br>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+</div>
+
 @stop
